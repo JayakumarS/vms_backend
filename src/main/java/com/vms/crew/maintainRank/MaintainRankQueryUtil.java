fe796ca0fd690bc3d23fb06f12f40951b5c9459a
Yuktavarshani <129827250+yuktavarshani@users.noreply.github.com>
2024-06-20 12:47:40 +0530

    changes

--- src/main/java/com/vms/crew/maintainRank/MaintainRankQueryUtil.java
+++ src/main/java/com/vms/crew/maintainRank/MaintainRankQueryUtil.java
@@ -2,19 +2,18 @@ package com.vms.crew.maintainRank;
 
 public class MaintainRankQueryUtil {
 
-	public static final String SAVE_VESSEL_TYPE = "Insert into rank_master (rank_id,rank_code,rank_name,dept_code,rank_group_code,ot_flag,rank_remarks,created_by,created_dt)\r\n"
-			+ "values (:sno,:code,:description,:department,:groupage,:oAndt,:remarks,:userName,now())";
+	public static final String SAVE_VESSEL_TYPE = "Insert into rank_master (rank_code,rank_name,dept_code,rank_group_code,ot_flag,rank_remarks,created_by,created_dt)\r\n"
+			+ "values (:code,:description,:department,:groupage,:oAndt,:remarks,:userName,now())";
 
-public static final String getList = "select rank_id as sno,rank_code as code,rank_name as description,dept_code as department,rank_group_code as groupage,ot_flag as oAndt,rank_remarks as remarks from rank_master order by created_dt desc";
+public static final String getList = "select rank_code as code,rank_name as description,dept_code as department,rank_group_code as groupage,ot_flag as oAndt,rank_remarks as remarks from rank_master order by created_dt desc";
 
-public static final String getEdit = "select rank_id as sno,rank_code as code,rank_name as description,dept_code as department,rank_group_code as groupage,ot_flag as oAndt,rank_remarks as remarks from rank_master where rank_code = ?";
+public static final String getEdit = "select rank_code as code,rank_name as description,dept_code as department,rank_group_code as groupage,ot_flag as oAndt,rank_remarks as remarks from rank_master where rank_code = ?";
 
 public static final String delete = "delete from rank_master where rank_code = ? ";
 
 public static final String checkDelete = "select count(*) from rank_master where rank_code = ?";
 
 public static final String UPDATE_VESSEL_TYPE = "UPDATE rank_master SET "
-	    + "rank_id = :sno, "
 	    + "rank_code = :code, "
 	    + "rank_name = :description, "
 	    + "dept_code = :department, "
