85b020375cfffddec68b9785a71d82ab091a10bf
Lavanyababu34 <babulavanya4594@gmail.com>
2024-06-21 14:43:02 +0530

    Query change done

--- src/main/java/com/vms/crew/utilities/MultiSeamen/MultiSeamenQueryUtil.java
+++ src/main/java/com/vms/crew/utilities/MultiSeamen/MultiSeamenQueryUtil.java
@@ -5,5 +5,5 @@ public class MultiSeamenQueryUtil {
 	public static final String SAVE_MULTI_SEAMEN_HDR = "Insert INTO multi_seamen_hdr (m_seamen_vessel_code,m_seamen_starting_date,m_seamen_joining_port) Values(:vessel,:startdate::Date,:joinPort)";
 
 	
-	public static final String SAVE_MULTI_SEAMEN_DTL = "Insert INTO multi_seamen_dtl (m_seamen_name,m_seamen_rank_code,m_seamen_pay,m_seamen_currency,m_seamen_joining_date,m_seamen_est_sign_off) Values(:name,:rank,:pay,:currency,:joiningDate::Date,:estSignOff::Date)";
+	public static final String SAVE_MULTI_SEAMEN_DTL = "Insert INTO multi_seamen_dtl (m_seamen_name,m_seamen_rank_code,m_seamen_pay,m_seamen_currency,m_seamen_joining_date,m_seamen_est_sign_off) Values(:name,:rank,:pay,:currency,to_date(:joiningDate,'DD/MM/YYYY'),to_date(:estSignOff,'DD/MM/YYYY'))";
 }
