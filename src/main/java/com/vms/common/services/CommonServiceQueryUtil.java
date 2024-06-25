8815eb3f7cce7b2c57ecc7bd8d8e67b6bca18629
Yuktavarshani <129827250+yuktavarshani@users.noreply.github.com>
2024-06-25 18:59:45 +0530

    changes

--- src/main/java/com/vms/common/services/CommonServiceQueryUtil.java
+++ src/main/java/com/vms/common/services/CommonServiceQueryUtil.java
@@ -20,11 +20,11 @@ public class CommonServiceQueryUtil {
 	
 	public static final String VESSEL_TYPE = "select vessel_type_code as id,vessel_type_name as text from vessel_type";
 	
-	public static final String VESSEL_INSURANCE = "select vessel_insurance_code as id,vessel_insurance_name as text from vessel_insurance";
+	public static final String VESSEL_INSURANCE = "select vessel_insurance_id as id,vessel_insurance_name as text from vessel_insurance";
 	
-	public static final String WAGE_SCALE = "select wage_scale_code as id,wage_scale_name as text from wage_scales";
+	public static final String WAGE_SCALE = "select wage_scale_id as id,wage_scale_name as text from wage_scales";
 	
-	public static final String GET_PORT = "select port_code as id,port_name as text from port_master";
+	public static final String GET_PORT = "select port_id as id,port_name as text from port_master";
 	
 	public static final String GET_COUNTRY = "select country_code as id,country_name as text, from country_master";
 
@@ -32,11 +32,11 @@ public class CommonServiceQueryUtil {
 
 	public static final String GET_PHONECODE = "select distinct phonecode as id,phonecode as text from country_master";
 		
-	public static final String GET_VESSEL_OWNER = "select vessel_owner_code as id,vessel_owner_name as text from vessel_owner";
+	public static final String GET_VESSEL_OWNER = "select vessel_owner_id as id,vessel_owner_name as text from vessel_owner";
 	
-	public static final String GET_OFFICIAL_MANAGERS = "select official_manager_code as id,official_manager_name as text from official_managers";
+	public static final String GET_OFFICIAL_MANAGERS = "select official_managers_id as id,official_manager_name as text from official_managers";
 	
-	public static final String GET_SHIP_MANAGERS = "select ship_manager_code as id,ship_manager_name as text from ship_managers";
+	public static final String GET_SHIP_MANAGERS = "select ship_managers_id as id,ship_manager_name as text from ship_managers";
 
 	public static final String GET_VESSEL = "select vessel_code as id,vessel_name as text from vessel_hdr";
 
