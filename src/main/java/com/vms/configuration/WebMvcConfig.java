c6c58d57b0ce5ee850b052a3c4adbac198796030
Yuktavarshani <129827250+yuktavarshani@users.noreply.github.com>
2024-06-26 12:45:55 +0530

    changes

--- src/main/java/com/vms/configuration/WebMvcConfig.java
+++ src/main/java/com/vms/configuration/WebMvcConfig.java
@@ -11,6 +11,12 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 @Configuration
 @EnableWebMvc
 public class WebMvcConfig implements WebMvcConfigurer {
+	
+	@Value("${export.files.absolutePath}")
+	private String filesExportAbsolutePath;
+	
+	@Value("${export.files.serverpath}")
+	private String filesExportServerPath;
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
@@ -19,7 +25,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
 	
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler(filesExportServerPath+"/**").addResourceLocations("file:///"+filesExportAbsolutePath);
+
 		//registry.addResourceHandler("CustomerDrugUpload/**").addResourceLocations("file:///root/drug_upload/");
 	}
-	
+
 }
